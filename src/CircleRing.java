93283f8530a62acf5de3ff97988f635a503ba48e
MitchellOeth <league@wts13.attlocal.net>
2018-03-25 11:25:35 -0700

    code4life, circle ring

--- src/CircleRing.java
+++ src/CircleRing.java
@@ -9,26 +9,35 @@ public class CircleRing {
     public static void main(String[] args) throws Exception {
 
         // 1. Make a new Robot
-
+Robot Mitch = new Robot();
 
        //  2. Set your robot’s position to x=150 and y=200
-
-        // 3. Put the robot's pen down
-
+Mitch.hide();
+Mitch.setX(150);
+Mitch.setY(200);
+		// 3. Put the robot's pen down
+Mitch.penDown();
         // 4. Set the robot’s speed to 10
-
+Mitch.setSpeed(10);
         // 5. Do everything below here 360 times (use i as the counter)
+for (int i = 0; i < args.length; i++) {
+	
 
 	        // 6. Move the robot 3 pixels
-
+Mitch.move(3);
        //  7. Turn the robot 1 degree
-
-        // 8. If the counter i is divisible by 20 (hint: use mod operator  %)..
+Mitch.turn(1);
+      // 8. If the counter i is divisible by 20 (hint: use mod operator  %)..
+if (i%20==0) {
+	for (Mitch
+	}
+}
 
 		// 9. do steps 10 and 11 exactly 360 times (use j as the counter)
 
 				// 10. Move the robot 1 pixel
 
 				// 11. Turn the robot 1 degree
+}
 }
 }
--- src/Code4Life.java
+++ src/Code4Life.java
@@ -7,18 +7,30 @@ import java.io.File;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.swing.JOptionPane;
 
 
 public class Code4Life {
-	/*
-     * Ask the user how many hours they spent coding this week.
-     * 
-     * 1. If it's 3 or more, tell them they're a Code Ninja.
-     * 
-     * 2. If it's less than 2, tell them to stop watching YouTube and write code instead.
-     * 
-     * 3. If it's more than 5, play the Batman theme song.
-     */
+	public static void main(String[] args) {
+		
+
+	 // Ask the user how many hours they spent coding this week.
+	String hours = JOptionPane.showInputDialog("How many hours have you spent coding this week?");
+int h = Integer.parseInt(hours);
+     // 1. If it's 3 or more, tell them they're a Code Ninja.
+     if (h>3) {
+    	 JOptionPane.showMessageDialog(null, "Your a code ninja");
+     }
+     // 2. If it's less than 2, tell them to stop watching YouTube and write code instead.
+     if (h<2) {
+    	 JOptionPane.showMessageDialog(null, "Stop watching YouTube and write code instead");
+     }
+     // 3. If it's more than 5, play the Batman theme song.
+     if (h>5) {
+     playBatmanTheme();
+     }
+	
+	}
 
 
     private static void playBatmanTheme() {
